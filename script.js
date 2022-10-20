1686bac6ac5e70c623283b5f5bb41c914feea0b4
Erick <erickmendesweb@gmail.com>
2022-10-20 17:24:47 -0300

    rotação 3d com imagens e backface-visibility feitas + função para criar carta

---
+++ script.js
@@ -0,0 +1,9 @@
+function cardCreate() {
+    const principal = document.querySelector(".main")
+    principal.innerHTML += `
+        <div class="card">
+            <div class="face front"></div>
+            <div class="face back">
+        </div>`
+    
+}
